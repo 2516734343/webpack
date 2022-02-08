4aa561aa76689783432676f57ad37c764d83f37b
2516734343 <2516734343@qq.com>
2022-02-08 22:30:38 +0800

    性能优化

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,29 @@
 module.exports = {
 
   mode: 'development',// production
+  devTool: 'source-map',
   entry: "./src/index.js",
   output: {
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /lodash/,
+        use: [
+          {
+            loader: "cache-loader",
+            options: {
+              cacheDirectory: "./cache"
+            }
+          },
+          "thread-loader",
+          "babel-loader"
+        ]
+      }
+    ],
+    noParse: /juqery/
   }
 
 }
